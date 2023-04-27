b9a03d81cc4ad3f92c55261ed7fe82ae26ead03e
Lii02 <lukeinlow@gmail.com>
2023-04-27 11:13:55 -0400

    Added dashboard shortcut, changed some colors, and tweaked SignInData class

--- backend/data.py
+++ backend/data.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from dataclasses import dataclass
 
+@dataclass
 class SignInData:
-    def __init__(self, first_name: str, last_name: str, timestamp: int, additional_notes: str):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.time_string = datetime.fromtimestamp(timestamp).strftime("%m/%d/%Y, %H:%M:%S")
-        self.additional_notes = additional_notes
+    first_name: str
+    last_name: str
+    timestamp: int
+    additional_notes: str
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} | {self.time_string} | {self.additional_notes}"
+    def get_time_string(self):
+        return datetime.fromtimestamp(self.timestamp).strftime("%m/%d/%Y, %H:%M:%S")
